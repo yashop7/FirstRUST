ab6ffdc7d9374f72fbef4a602cffc6aa479017d1
Yash <139779840+yashop7@users.noreply.github.com>
2024-12-19 16:02:45 +0530

    fromterminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+
+struct User {
+    name: String,
+    age : u32,
+    active: bool
+}
 fn main() {
-    println!("Hello, world!");
+    let name = String::from("John Doe");
+    let user = User {
+        name,
+        age: 25,
+        active: true
+    };
+    print!("User: {} is {} years old and is active: {}", user.name, user.age, user.active);
 }
